853157cb53114390171541a27c0a9f2e6b5b4052
V.Piraisoodan <piraisoodanv1996@gmail.com>
2023-01-24 01:10:11 +0530

    img alt property added

--- src/components/Aboutme/About.js
+++ src/components/Aboutme/About.js
@@ -7,13 +7,13 @@ const About = () => {
     <Element className='about-container' name="about">
       <div className='about-sub'>
         <h1>About</h1>
-        <p><span><img className='about-handimg' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> Hello! I'm V.Piraisoodan,a passionate software engineer.
-          <br/><span><img className='about-handimg' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> I develop Full Stack web applications.
+        <p><span><img className='about-handimg' alt='handicon' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> Hello! I'm V.Piraisoodan,a passionate software engineer.
+          <br/><span><img className='about-handimg' alt='handicon' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> I develop Full Stack web applications.
           My core skill is based on Javascript.
-          <br/><span><img className='about-handimg' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> I would describe myself as a quick-learner,positive and confident individual.
+          <br/><span><img className='about-handimg' alt='handicon' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> I would describe myself as a quick-learner,positive and confident individual.
           Passionate towards problem solving.
-          <br/><span><img className='about-handimg' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> I completed my B.E ECE in Arunai Engineering College in 2017.
-          <br/><span><img className='about-handimg' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> Looking for a job, where I can enhance my learning and utilize my skills.</p>
+          <br/><span><img className='about-handimg' alt='handicon' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> I completed my B.E ECE in Arunai Engineering College in 2017.
+          <br/><span><img className='about-handimg' alt='handicon' src='https://cdn-icons-png.flaticon.com/512/556/556130.png'></img></span> Looking for a job, where I can enhance my learning and utilize my skills.</p>
         <p className='about-course'>
           <h1>Course</h1>
           <p><span className='mern'>MERN</span> Full Stack Development - (2022-Present)<br/>
